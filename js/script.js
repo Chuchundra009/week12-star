e3337d561e2b7f1eef311d0967ebd3e22b4fd73e
Chuchundra009 <redfox161294@gmail.com>
2023-09-03 16:45:26 +0300

    new week

--- js/script.js
+++ js/script.js
@@ -1,29 +1,99 @@
-const textName = document.querySelector('#yourName');
-const srcAva = document.querySelector('#yourAva');
-const yourComment = document.querySelector('#yourComment');
+
 const btn = document.querySelector('#btn');
-const res = document.querySelector('#res');
-const nameRes = document.querySelector('#nameRes');
-const comRes = document.querySelector('#comRes');
 
 
-let chatInfo = () => {
+
+const seeYes = document.querySelector('#seeYes');
+const seeNO = document.querySelector('#seeNo');
+const seeName = document.querySelector('#seeName');
+
+
+
+
+seeNO.addEventListener('click', () => {
+    const valueNo = seeNO.value;
+
+    valueNo=== 'on'? seeName.style.display = 'none':seeName.style.display = 'block';
+
+})
+
+seeYes.addEventListener('click', () => {
+    const valueYes = seeYes.value;
+
+    valueYes==='on'? seeName.style.display = 'flex':seeName.style.display = 'block';
+
+})
+
+
+function chatInfo() {
+
+    const textName = document.querySelector('#yourName');
     const nameLow = textName.value.replaceAll(' ', '').toLowerCase();
-    const newName = nameLow[0].toUpperCase() + nameLow.slice(1);
-    nameRes.innerHTML = newName;
     textName.value = '';
+    let newname;
+    let namestyle = '';
+
+    if(nameLow==''){
+        newname = 'Username';
+        namestyle = 'color:violet'
+    }else{
+        newname = nameLow[0].toUpperCase() + nameLow.slice(1);
+    }
 
 
+
+    const quotes = ['../img/eff.jpg','../img/iii.jpg','../img/kenni.jpg','../img/kkk.jpg','../img/multfilm.jpg','../img/rrr.jpg'];
+
+
+    const srcAva = document.querySelector('#yourAva');
     const infoSrc = srcAva.value;
-    const newImg = document.createElement('img');
-    res.prepend(newImg);
-    newImg.src = infoSrc;
-    newImg.style.width = '150px';
     srcAva.value = '';
 
+    let imgimg;
 
-    comRes.innerHTML = yourComment.value.toLowerCase().replace('viagra', '***').replace('xxx', '***').replace('ххх', '***');
+    if(!infoSrc.length){
+        let randomImg = quotes[Math.floor(Math.random()*quotes.length)];
+        imgimg = randomImg;
+    }else{
+        imgimg = infoSrc;
+    }
+
+    const yourComment = document.querySelector('#yourComment');
+    let commpepl = yourComment.value.toLowerCase().replace('viagra', '***').replace('xxx', '***').replace('ххх', '***');
     yourComment.value = '';
+
+
+    const date = new Date();
+    const newDate = date.toDateString();
+    const newHour = date.getHours();
+    const newMin = date.getMinutes();
+    const newSec = date.getSeconds();
+
+
+    const seeTimes = `${newDate} at ${newHour}:${newMin}:${newSec}`;
+
+
+    let newMsg = `
+    <div class="chatList__answer">
+        <div class="cont-res">
+            <div class="position-res">
+                <img src="${imgimg}" alt="" style="width:150px;">
+                <p style=${namestyle}>${newname}</p>
+            </div>
+            <p>${commpepl}</p>
+        </div>
+        <div class="cont__time">
+            <p class="seeTime">${seeTimes}</p>
+        </div>
+    </div>
+    `;
+    let chat = document.getElementById('resList');
+    chat.innerHTML = newMsg + chat.innerHTML;
+
 }
 
 btn.addEventListener("click", chatInfo);
+
+
+
+
